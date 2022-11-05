dead7c0d7eaa6e32ade134d59426fe16fc0dc6c1
hungd <hungd@DESKTOP-51T5371>
2022-11-05 16:27:15 +0700

    Add project files.

---
+++ SocialMedia/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMedia;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SocialMediaTests.Unit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SocialMediaTests.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
