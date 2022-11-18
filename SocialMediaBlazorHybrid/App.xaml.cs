a73b2dde4bea4a3c9c7429f5c2285cbbc04de3d3
Dương Quang Hưng <hungduong150698@gmail.com>
2022-11-18 21:26:30 +0700

    Remove socialmedia add socialmediahybrid dontet 7

---
+++ SocialMediaBlazorHybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMediaBlazorHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SocialMediaBlazorHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMediaBlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
