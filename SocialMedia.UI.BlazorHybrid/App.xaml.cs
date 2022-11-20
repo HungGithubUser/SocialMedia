f4807f596aa4b5517b25ea8454c635fe01c5c73c
Dương Quang Hưng <hungduong150698@gmail.com>
2022-11-20 17:15:57 +0700

    Rename Blazor.UI.Hybrid, add infrastructure layer

---
+++ SocialMedia.UI.BlazorHybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMedia.UI.BlazorHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SocialMedia.UI.BlazorHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.UI.BlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
