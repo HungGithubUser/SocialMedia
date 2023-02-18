464e7155d40bfc627b6633b18c67432eb8342e7e
hung.duong <hung.duong@ansarada.com>
2023-02-18 16:10:44 +0700

    Local development

--- SocialMedia.Api/Program.cs
+++ SocialMedia.Api/Program.cs
@@ -6,10 +6,9 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.MapFallbackToFile("index.html");
 app.Run();
